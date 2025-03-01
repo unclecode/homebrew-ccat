be78c4ace7183a750d393bc93dff3715185d2aba
unclecode <unclecode@kidocode.com>
2025-03-01 22:12:11 +0800

    Fix class name in codecat formula

--- Formula/codecat.rb
+++ Formula/codecat.rb
@@ -1,6 +1,6 @@
-class Ccat < Formula
+class Codecat < Formula
   desc "CLI tool to clone and concatenate code repositories for AI analysis"
-  homepage "https://github.com/unclecode/ccat"
+  homepage "https://github.com/unclecode/ccat4ai"
   url "https://github.com/unclecode/ccat4ai/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "9e796975808b363321f4ac972ce53c5ffaf41289219b4b4e47ef1cb8915e452f"
   license "MIT"
@@ -11,10 +11,12 @@ class Ccat < Formula
   depends_on "file"
 
   def install
-    bin.install "ccat.sh" => "ccat"
+    bin.install "ccat.sh" => "codecat"
+    # Create a symlink so users can use either 'codecat' or 'ccat4ai'
+    bin.install_symlink "codecat" => "ccat4ai"
   end
 
   test do
-    system "#{bin}/ccat", "--version"
+    system "#{bin}/codecat", "--version"
   end
 end
