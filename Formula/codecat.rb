cd5bfcab1800439316c27aab016d514319397cb9
unclecode <unclecode@kidocode.com>
2025-03-01 22:17:30 +0800

    Update SHA256 checksum for v0.1.0

--- Formula/codecat.rb
+++ Formula/codecat.rb
@@ -2,7 +2,7 @@ class Codecat < Formula
   desc "CLI tool to clone and concatenate code repositories for AI analysis"
   homepage "https://github.com/unclecode/ccat4ai"
   url "https://github.com/unclecode/ccat4ai/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "9e796975808b363321f4ac972ce53c5ffaf41289219b4b4e47ef1cb8915e452f"
+  sha256 "9c42c0f98a4ee2e8b9cd9e699963259f13a79dc586037ce9315b390b0aab020c"
   license "MIT"
 
   depends_on "git"
